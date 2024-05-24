e816cd1717edf15ea4a6bbbb14bb2a26f8ef2c4b
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-05-24 13:40:33 +0300

    1

---
+++ src/js/api-key.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+export const JSON_API_URL = 'https://pixabay.com/api/';
+export const PIXABAY_API_KEY = '44045744-87391f93bf3caee56476bbdd7';
+
+export const options = {
+    params: {
+        key = PIXABAY_API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        q:'',
+    }
+};
---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import simpleLightbox from "simplelightbox";
+import { Notify } from "notiflix";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { options, JSON_API_URL,PIXABAY_API_KEY } from "./api-key";
+const searchForm = document.querySelector('.search-form');
+const imageGallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+loadBtn.style.display = 'none';
+Notify.info("We're sorry, but you've reached the end of search results.");
+Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+function onSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    options.params.q = form.element.searchQuery.value;
+    if (searchQuery === "") {
+        Notify.info('Please fill in the search input');
+        return;
+    }
+}
+searchForm.addEventListener('submit', onSubmit)
