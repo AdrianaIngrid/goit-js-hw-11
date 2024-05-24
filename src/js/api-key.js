df9a7befbab5aebdf19aafb6fa9fd0e5c253a50f
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-05-24 17:04:11 +0300

    g

--- src/js/api-key.js
+++ src/js/api-key.js
@@ -4,10 +4,10 @@ export const PIXABAY_API_KEY = '44045744-87391f93bf3caee56476bbdd7';
 
 export const options = {
     params: {
-        key = PIXABAY_API_KEY,
+        key : PIXABAY_API_KEY,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
         q:'',
-    }
+    },
 };
