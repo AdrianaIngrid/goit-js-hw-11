9b8c52dfd2255c038d349229c4079bdda9c94d49
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-05-25 19:32:34 +0300

    75

--- src/js/api-key.js
+++ src/js/api-key.js
@@ -1,13 +1,15 @@
-import axios from "axios";
+
 export const JSON_API_URL = 'https://pixabay.com/api/';
 export const PIXABAY_API_KEY = '44045744-87391f93bf3caee56476bbdd7';
 
 export const options = {
     params: {
-        key : PIXABAY_API_KEY,
+        key: PIXABAY_API_KEY,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: 40,
+        page:1,
         q:'',
     },
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,86 @@
-import simpleLightbox from "simplelightbox";
-import { Notify } from "notiflix";
+import axios from 'axios';
+import { options, JSON_API_URL } from './api-key';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { options, JSON_API_URL, PIXABAY_API_KEY } from "./api-key";
 
 const searchForm = document.querySelector('.search-form');
-//const imageGallery = document.querySelector('.gallery');
+const imagesGallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
+
 loadBtn.style.display = 'none';
-Notify.info("We're sorry, but you've reached the end of search results.");
-Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-function onSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    options.params.q = form.element.searchQuery.value;
-    if (searchQuery === "") {
-        Notify.info('Please fill in the search input');
-        return;
+const gallery = () =>
+  new SimpleLightbox('.photo-card a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+function getPhotos(hits) {
+  const markUp = hits
+    .map(item => {
+      return `<div class="photo-card">
+            <a href ="${item.largeImageURL}">
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+                </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> </br> ${item.likes}</p>
+    <p class="info-item">
+      <b>Views</b> </br> ${item.views}</p>
+    <p class="info-item">
+      <b>Comments</b></br> ${item.comments}</p>
+    <p class="info-item">
+      <b>Downloads</b> </br> ${item.downloads}</p>
+  </div>
+</div>`;
+    })
+    .join('');
+  imagesGallery.insertAdjacentHTML('beforeend', markUp);
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  options.params.q = form.elements.searchQuery.value;
+  if (options.params.q === '') {
+    Notify.info('Please fill in the search input!');
+    imagesGallery.innerHTML = '';
+    return;
+  }
+  options.params.page = 1;
+  imagesGallery.innerHTML = '';
+  try {
+    const response = await axios.get(JSON_API_URL, options);
+    const totalHits = response.data.totalHits;
+    const hits = response.data.hits;
+    if (hits.length === 0) {
+      loadBtn.style.display = 'none';
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      getPhotos(hits);
+      gallery();
+      loadBtn.style.display = 'block';
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function getMorePhotos() {
+    options.params.page += 1;
+    try {
+        const response = await axios.get(JSON_API_URL, options);
+        const hits = response.data.hits;
+        const totalHits = response.data.totalHits;
+        getPhotos(hits);
+        if (options.params.page * options.params.per_page >= totalHits) {
+            loadBtn.style.display = 'none';
+            Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
-searchForm.addEventListener('submit', onSubmit)
+searchForm.addEventListener('submit', onSubmit);
+loadBtn.addEventListener('click', getMorePhotos);
